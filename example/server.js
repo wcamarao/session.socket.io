13ffbfca40b012fa5811350f15eeb88786f6a4d0
Wagner Camarao <wcamarao@rocketfuel.com>
2013-01-31 17:38:25 -0800

    Usage example

---
+++ example/server.js
@@ -0,0 +1,42 @@
+var http = require('http')
+  , path = require('path')
+  , connect = require('connect')
+  , express = require('express')
+  , app = express();
+
+var cookieParser = express.cookieParser('your secret sauce')
+  , sessionStore = new connect.middleware.session.MemoryStore();
+
+app.configure(function () {
+  app.set('views', path.resolve('views'));
+  app.set('view engine', 'jade');
+
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(cookieParser);
+  app.use(express.session({ store: sessionStore }));
+  app.use(app.router);
+});
+
+var server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+var SessionSockets = require('session.socket.io')
+  , sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
+
+app.get('/', function(req, res) {
+  req.session.foo = req.session.foo || 'bar';
+  res.render('index');
+});
+
+sessionSockets.on('connection', function (err, socket, session) {
+  socket.emit('session', session);
+
+  socket.on('foo', function(value) {
+    session.foo = value;
+    session.save();
+    socket.emit('session', session);
+  });
+});
+
+server.listen(3000);
