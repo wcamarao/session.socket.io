e2afa806e992d8c324b98cf56965400dea8997fe
Wagner Camarao <functioncallback@gmail.com>
2013-06-29 09:39:54 -0700

    minor refactoring: renames and formatting

--- session.socket.io.js
+++ session.socket.io.js
@@ -1,29 +1,20 @@
 module.exports = SessionSockets;
 
 function SessionSockets(io, sessionStore, cookieParser, key) {
-  key = key || 'connect.sid';
+  key = typeof key === 'undefined' ? 'connect.sid' : key;
+  var sessionSockets = this;
 
-  var self = this;
+  this.on = function(event, callback) {
+    return bind(event, callback, io.sockets);
+  };
 
   this.of = function(namespace) {
     return {
       on: function(event, callback) {
-        return bind(self.getSession, event, callback, io.of(namespace));
+        return bind(event, callback, io.of(namespace));
       }
     };
   };
-
-  this.on = function(event, callback) {
-    return bind(self.getSession, event, callback, io.sockets);
-  };
-
-  function bind(getsession, event, callback, namespace) {
-    namespace.on(event, function(socket) {
-      getsession(socket, function (err, session) {
-        callback(err, socket, session);
-      });
-    });
-  }
 
   this.getSession = function(socket, callback) {
     cookieParser(socket.handshake, {}, function (parseErr) {
@@ -34,10 +25,19 @@ function SessionSockets(io, sessionStore, cookieParser, key) {
     });
   };
 
+  function bind(event, callback, namespace) {
+    namespace.on(event, function (socket) {
+      sessionSockets.getSession(socket, function (err, session) {
+        callback(err, socket, session);
+      });
+    });
+  }
+
   function findCookie(handshake) {
-    if (handshake) return (handshake.secureCookies && handshake.secureCookies[key])
-                       || (handshake.signedCookies && handshake.signedCookies[key])
-                       || (handshake.cookies && handshake.cookies[key]);
+    if (handshake)
+      return (handshake.secureCookies && handshake.secureCookies[key]) ||
+             (handshake.signedCookies && handshake.signedCookies[key]) ||
+             (handshake.cookies && handshake.cookies[key]);
   }
 
   function resolveErr(parseErr, storeErr, session) {
